7d40b2dc707965cd17492ce6392855ed9925d800
johannpa <johann_paque@hotmail.fr>
2021-04-08 20:26:47 +0200

    next episode

---
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'al-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styles: [
+  ]
+})
+export class PageNotFoundComponent {
+
+  constructor(private location: Location) { }
+
+  back() {
+    this.location.back();
+  }
+
+
+}
