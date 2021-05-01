12a73f3f46893380ff58372d2212308a66b9ba58
johannpa <johann_paque@hotmail.fr>
2021-05-01 21:03:04 +0200

    news components

---
+++ src/app/protected/workday/workday-form/workday-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'al-workday-form',
+  templateUrl: './workday-form.component.html',
+  styles: [
+  ]
+})
+export class WorkdayFormComponent implements OnInit {
+
+  workdayForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.workdayForm = this.createWorkdayForm()
+  }
+
+  get dueDate() { return this.workdayForm.get('dueDate'); }
+  get notes() { return this.workdayForm.get('notes'); }
+  get tasks() { return this.workdayForm.get('tasks') as FormArray; }
+
+  createWorkdayForm(): FormGroup {
+    return this.fb.group({
+      'dueDate': '',
+      'tasks': this.fb.array([]),
+      'notes': '',
+    });
+  }
+
+  submit(): void {
+    console.info(this.workdayForm.value);
+  }
+
+}
